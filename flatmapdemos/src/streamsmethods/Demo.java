953fe66353d1b6cb92ac7e7bb926dab9a832a3c3
MahabirGupta <mahabirg81@gmail.com>
2024-09-24 22:35:17 +0800

    using streams add the collections to the stream and use sorted(Comparator.reverseOrder()) method to sort the integerList in decending order

--- flatmapdemos/src/streamsmethods/Demo.java
+++ flatmapdemos/src/streamsmethods/Demo.java
@@ -1,6 +1,7 @@
 package streamsmethods;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,11 @@ public class Demo {
 
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(2,4,3,7,5,8);
-//        Using sorted() method to sort the integerList
+//        Using sorted() method to sort the integerList in ascending order
       List<Integer> newIntegerList = integerList.stream().sorted().collect(Collectors.toList()); //Sorting ascending order
         System.out.println(newIntegerList);
+//        Using sorted(Comparator.reverseOrder()) method to sort the integerList in decending order
+        List<Integer> newIntegerList1 = integerList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()); //Sorting ascending order
+        System.out.println(newIntegerList1);
     }
 }
