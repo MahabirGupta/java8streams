cf3b5d68bff2ea0d162be1366789e1488163b0d8
MahabirGupta <mahabirg81@gmail.com>
2024-09-24 14:40:19 +0800

    using streams add the collections to the stream and use min() to find the minimum number in the numberList.

--- flatmapdemos/src/Demo4.java
+++ flatmapdemos/src/Demo4.java
@@ -1,14 +1,26 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Demo4 {
 
     public static void main(String[] args) {
         List<Integer> numberList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-//        using count to count the number of even numbers in the numberList
+//        using count() to count the number of even numbers in the numberList
       long numOfEvenNumbers = numberList.stream().filter(num -> (num%2 == 0)).count();
         System.out.println("Number of even numbers = " + numOfEvenNumbers);
+
+//        using min() have to pass 2 arguments
+      Optional<Integer> min =  numberList.stream().min((val1, val2) ->{
+            return val1.compareTo(val2);
+        });
+
+//      to print the minimum value
+        System.out.println("Minimum number = "+min.get());
+
+
+
 
     }
 }
